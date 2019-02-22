dcb1236ee0eee8446cace48e9b894ce0c2cdf481
Scythian <scythianfuego@gmail.com>
2019-02-22 06:32:59 +0000

    arcs simplified

--- src/objects.ts
+++ src/objects.ts
@@ -47,90 +47,56 @@ export default class Objects {
     return { sx, sy, ex, ey, type: 0x10 + index };
   }
 
-  longArc(hex: Hex, index: number): IRailArc {
+  baseArc(
+    hex: Hex,
+    index: number,
+    radius: number,
+    arc: number,
+    baseType: number,
+    outer: boolean
+  ) {
     const corners = this.getTriangleCorners(hex);
-    const pairs = [[2, 0], [2, 1], [0, 1], [0, 2], [1, 2], [1, 0]];
-    const arc = Math.PI / 3;
+    const points = outer ? [2, 2, 0, 0, 1, 1] : [1, 0, 2, 1, 0, 2];
 
-    const rot1 = (2 / 3) * Math.PI; // one third of the circle - 120 deg
+    const rot1 = (2 / 3) * Math.PI;
     const rot2 = (4 / 3) * Math.PI;
-    const vectors = [0, 0, rot1, rot1, rot2, rot2];
+    const rotation = [0, 0, rot1, rot1, rot2, rot2];
 
+    const sign = outer ? -1 : 1;
     const angles = [0, 1, 2, 3, 4, 5]
-      .map(v => Math.PI / 2 + vectors[v])
+      .map(v => (sign * Math.PI) / 2 + rotation[v])
       .map((v, i) => (i % 2 === 0 ? [v - arc, v] : [v, v + arc]));
 
-    const [a, b] = pairs[index];
+    const p = points[index];
     const [a1, a2] = angles[index];
-    const radius = 6 * hex.size;
-    const center = corners[a].add(
-      corners[a].subtract(corners[b]).multiply(3, 3)
-    );
+    const center = corners[p].add({
+      x: sign * radius * Math.sin(rotation[index]),
+      y: -sign * radius * Math.cos(rotation[index])
+    });
     const { x, y } = center;
 
     const sx = x + radius * Math.cos(a1);
     const sy = y + radius * Math.sin(a1);
     const ex = x + radius * Math.cos(a2);
     const ey = y + radius * Math.sin(a2);
-    return { x, y, radius, a1, a2, sx, sy, ex, ey, type: 0x20 + index };
+    return { x, y, radius, a1, a2, sx, sy, ex, ey, type: baseType + index };
+  }
+
+  longArc(hex: Hex, index: number): IRailArc {
+    const radius = 6 * hex.size;
+    const arc = Math.PI / 3;
+    return this.baseArc(hex, index, radius, arc, 0x20, false);
   }
 
   shortArc(hex: Hex, index: number): IRailArc {
-    const corners = this.getTriangleCorners(hex);
-    const points = [1, 0, 2, 1, 0, 2];
     const radius = 3.5 * Math.sqrt(3); //3.5 * hex.size; // magic1
     const arc = 0.3802512067; // 1 / 3; magic2
-
-    const rot1 = (2 / 3) * Math.PI;
-    const rot2 = (4 / 3) * Math.PI;
-    const vectors = [0, 0, rot1, rot1, rot2, rot2];
-
-    const angles = [0, 1, 2, 3, 4, 5]
-      .map(v => Math.PI / 2 + vectors[v])
-      .map((v, i) => (i % 2 === 0 ? [v - arc, v] : [v, v + arc]));
-
-    const p = points[index];
-    const [a1, a2] = angles[index];
-    const center = corners[p].add({
-      x: radius * Math.sin(vectors[index]),
-      y: -radius * Math.cos(vectors[index])
-    });
-    const { x, y } = center;
-
-    const sx = x + radius * Math.cos(a1);
-    const sy = y + radius * Math.sin(a1);
-    const ex = x + radius * Math.cos(a2);
-    const ey = y + radius * Math.sin(a2);
-    return { x, y, radius, a1, a2, sx, sy, ex, ey, type: 0x30 + index };
+    return this.baseArc(hex, index, radius, arc, 0x30, false);
   }
 
   shortArc2(hex: Hex, index: number): IRailArc {
-    const corners = this.getTriangleCorners(hex);
-    const points = [2, 2, 0, 0, 1, 1];
     const radius = 3.5 * Math.sqrt(3); // 3.5 * hex.size; // magic1
     const arc = 0.3802512067; // 2*Math.acos(9 / sqrt(84)) //1 / 3; // magic2
-
-    const rot1 = (2 / 3) * Math.PI;
-    const rot2 = (4 / 3) * Math.PI;
-    const vectors = [0, 0, rot1, rot1, rot2, rot2];
-
-    const angles = [0, 1, 2, 3, 4, 5]
-      .map(v => -Math.PI / 2 + vectors[v])
-      .map((v, i) => (i % 2 === 0 ? [v - arc, v] : [v, v + arc]));
-    // .map(v => [0, 6]);
-
-    const p = points[index];
-    const [a1, a2] = angles[index];
-    const center = corners[p].add({
-      x: -radius * Math.sin(vectors[index]),
-      y: radius * Math.cos(vectors[index])
-    });
-    const { x, y } = center;
-
-    const sx = x + radius * Math.cos(a1);
-    const sy = y + radius * Math.sin(a1);
-    const ex = x + radius * Math.cos(a2);
-    const ey = y + radius * Math.sin(a2);
-    return { x, y, radius, a1, a2, sx, sy, ex, ey, type: 0x40 + index };
+    return this.baseArc(hex, index, radius, arc, 0x40, true);
   }
 }
